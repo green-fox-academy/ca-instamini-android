c82b290a47602f7d4e1100b9dd050f3ae94099ab
Adam Gyulavari <adam.gyulavari@lab.coop>
2018-03-22 12:48:59 +0100

    implement login screen basic structure

--- app/src/main/java/com/greenfox/aze/instamini/Login.java
+++ app/src/main/java/com/greenfox/aze/instamini/Login.java
@@ -2,12 +2,27 @@ package com.greenfox.aze.instamini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    EditText username;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username = findViewById(R.id.username);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Login.this, username.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
